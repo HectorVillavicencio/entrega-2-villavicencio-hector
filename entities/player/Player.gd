27f5665a26b4fe26348b41bb2feef7c4fbc9e232
HectorVillavicencio <103118349+HectorVillavicencio@users.noreply.github.com>
2022-05-04 21:12:06 -0300

    agregada la friccion

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -2,8 +2,12 @@ extends Sprite
 
 onready var  cannon:Sprite = $Canion
 
-var speed = 200 #Pixeles
+export (float) var ACCELERATION: float = 20.0
+export (float) var H_SPEED_LIMIT:float  = 600.0
+export (float) var FRICTION_WEIGHT:float = 0.1
 
+var velocity : Vector2 = Vector2.ZERO
+var speed = 200 #Pixeles
 var projectile_container:Node
 
 
@@ -12,9 +16,7 @@ func set_projectile_container(container:Node):
 	cannon.projectile_container = container
 	projectile_container = container
 
-func _physics_process(delta):
-	var direction_optimized:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	
+func _physics_process(delta):	
 	var mouse_position:Vector2 = get_global_mouse_position()
 	cannon.look_at(mouse_position)
 	#var origen: Vector2 = global_position -- version larga
@@ -23,6 +25,13 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("fire"):
 		cannon.fire()
+		
+		
+	var direction_optimized:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+		
+	if direction_optimized != 0:
+		velocity.x = clamp(velocity.x + (direction_optimized * ACCELERATION), -H_SPEED_LIMIT, H_SPEED_LIMIT)
+	else:
+		velocity.x = lerp(velocity.x, 0, FRICTION_WEIGHT) if abs(velocity.x) > 1 else 0
 	
-	
-	position.x += direction_optimized * speed * delta
+	position += velocity * delta
